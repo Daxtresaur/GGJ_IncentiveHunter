37eed5dfe3c42872ccfe565d5ab83832dfa15764
Nathan-Macabanti <73865062+Nathan-Macabanti@users.noreply.github.com>
2023-02-03 21:53:45 +0800

    Movement

--- GGJ Lez Get It/Assets/PlayerController.cs
+++ GGJ Lez Get It/Assets/PlayerController.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Vector2 movement;
+    private Rigidbody rb;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnMovement(InputValue value)
     {
-        
+        movement = value.Get<Vector2>();
     }
 }
