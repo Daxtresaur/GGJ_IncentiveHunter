c47f74553174c91e99bc766e1bb40e0cb0969c2e
Janarci <kh4lilj4n@gmail.com>
2023-02-04 00:28:53 +0800

    monster

---
+++ GGJ Lez Get It/Assets/MonsterBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBehavior : MonoBehaviour
+{
+
+    public GameObject player; // get player position
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
