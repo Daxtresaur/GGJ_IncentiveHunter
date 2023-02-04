de7de5075a4934cd23d6074d5417a8b11dfb8e20
Janarci <kh4lilj4n@gmail.com>
2023-02-05 01:42:13 +0800

    menu

--- GGJ Lez Get It/Assets/Scripts/blackout.cs
+++ GGJ Lez Get It/Assets/Scripts/blackout.cs
@@ -8,12 +8,15 @@ public class blackout : MonoBehaviour
 {
 
 	public CanvasGroup canvGroup;
+	public Image image;
 	public float duration = 5f;
 	private bool mFaded = false;
+	public AudioSource treeChop;
 
 	private void Start()
 	{
-		
+		StartCoroutine(DoFadeWin(canvGroup, canvGroup.alpha, mFaded ? 1 : 0));
+
 	}
 	public void Fade()
 	{
@@ -37,6 +40,46 @@ public class blackout : MonoBehaviour
 		}
 		//lipat scene
 		SceneManager.LoadScene(sceneName: "PreStartScene");
+
+	}
+
+	public void FadeWin()
+	{
+
+		StartCoroutine(DoFadeWin(canvGroup, canvGroup.alpha, mFaded ? 1 : 0));
+
+		mFaded = !mFaded;
+	}
+
+	public IEnumerator DoFadeWin(CanvasGroup canvGroup, float start, float end)
+	{
+		float counter = 0f;
+		while (counter < duration)//fade to black
+		{
+
+			counter += Time.deltaTime;
+			canvGroup.alpha = Mathf.Lerp(start, 1, counter / duration);
+
+			yield return null;
+		}
+		
+		treeChop.Play();
+		yield return new WaitWhile(() => treeChop.isPlaying);
+
+
+		counter = 0f;
+		var tempColor = image.color;
+
+		while (counter < duration)//fade to white
+		{
+
+			counter += Time.deltaTime;
+			tempColor = Color.Lerp(Color.black, Color.white, counter / duration);
+			image.color = tempColor;
+
+			yield return null;
+		}
+
 
 	}
 
